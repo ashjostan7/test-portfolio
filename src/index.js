c662d88e0071ca05c972f005b26713803dcf0991
ashjostan7 <ashjostan7@gmail.com>
2020-07-20 16:29:45 +0100

    linking project assets

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function loadProjects(){
 
     
     for (var i = 0; i <data.projects.length; i++) {
-        var {project_id,date,heading,tags,description,image_link,awards,is_report,report_link,is_code,code_link}=data.projects[i];
+        var {project_id,date,heading,tags,description,image_link,awards,is_report,report_link,is_code,code_link,is_video,video_link}=data.projects[i];
         
         //Creating the tags for each project card
         var tag_list="";
@@ -22,8 +22,20 @@ function loadProjects(){
             tag_list=tag_list+'<p>'+tags[j]+'</p>'
         }
         
-        var section1 ='<div class="date">'+date+'</div><div class="header"><h5>'+heading+'</h5><div class="tags">'+tag_list+'</div>'
-        $('<div class="content">'+section1+'<div class="description"><img src='+image_link+'><p>'+description+'</p></div> <div class=report><div class="report_data"><a href="mailto:ashjostan7@gmail.com">Read Report</a></div></div></div>').appendTo(".project_section");
+        var section1 ='<div class="date">'+date+'</div><div class="header"><h5>'+heading+'</h5><div class="tags">'+tag_list+'</div>';
+        var section2='<img src='+image_link+'><p>'+description+'</p>';
+        var section3='';
+        if (is_report=="1"){
+             section3='<div><a href='+report_link+' target="blank">Read Report</a></div>'
+        }
+        if (is_code=="1"){
+            section3=section3+'<div><a href='+code_link+'target="blank">View Code</a></div>'
+        }
+        if (is_video=="1"){
+            section3=section3+'<div><a href='+video_link+'target="_blank">View Video</a></div>'
+        }
+        
+        $('<div class="content">'+section1+'<div class="description">'+section2+'</div> <div class=report>'+section3+'</div></div>').appendTo(".project_section");
     }
     $('.project_section').slick({
         // centerMode: true,
